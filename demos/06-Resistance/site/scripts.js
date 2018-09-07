8508967b8c67e730269a372aaed781b5e6b33be5
Daniel Powers <dancepowers@gmail.com>
2018-09-07 15:36:21 +0200

    resistance data update

--- demos/06-Resistance/site/scripts.js
+++ demos/06-Resistance/site/scripts.js
@@ -1,13 +1,18 @@
 let _data = false;
+let _images = false;
 let _state = false;
 let _municipality = false;
 let _group = false;
 let _numParticipants;
-let _numMonths;
+let _numDays;
+let _material;
+let _damage;
 
 updateScreen();
 fetchData();
 createForm();
+
+
 
 function fetchData() {
 	fetch(`${window.location.protocol}//${window.location.hostname}:${window.location.port}/blockades.json`, {
@@ -23,6 +28,16 @@ function fetchData() {
 	});
 }
 
+fetch(`${window.location.protocol}//${window.location.hostname}:${window.location.port}/images.json`, {
+	method: 'get'
+}).then((response) => {
+	return response.json();
+}).then(function(data) {
+	_images = data;
+}).catch((err) => {
+	console.error(err)
+});
+
 
 function createForm() {
 
@@ -34,9 +49,22 @@ function createForm() {
 	document.querySelector('form').addEventListener('submit', (e) => {
 		e.preventDefault();
 		const numParticipants = parseInt(document.querySelector('#num-participants').value);
-		const numMonths = parseInt(document.querySelector('#num-months').value);
+		const numDays = parseInt(document.querySelector('#num-days').value);
+		const material = document.querySelector('#material').value;
+
 		_numParticipants = numParticipants;
-		_numMonths = numMonths;
+		_numDays = numDays;
+		_material = material;
+
+		const materialAmounts = {
+			trucks: 12999,
+			tires: 1272,
+			people: 59,
+			barriers: 124,
+		};
+
+		_damage = materialAmounts[_material] * numDays * numParticipants;
+
 		updateScreen('results');
 	})
 }
@@ -51,19 +79,23 @@ function updateScreen(screen) {
 
 	if (screen === 'state') {
 		document.querySelector('main#selection').classList.remove('hidden');
+		document.querySelector('#about').classList.remove('hidden');
 		document.querySelector('#states').classList.remove('hidden');
 		document.querySelector('form').reset();
 	} else if (screen === 'municipality') {
 		document.querySelector('main#selection').classList.remove('hidden');
+		document.querySelector('#about').classList.remove('hidden');
 		document.querySelector('#states').classList.remove('hidden');
 		document.querySelector('#municipalities').classList.remove('hidden');
 	} else if (screen === 'group') {
 		document.querySelector('main#selection').classList.remove('hidden');
+		document.querySelector('#about').classList.remove('hidden');
 		document.querySelector('#states').classList.remove('hidden');
 		document.querySelector('#municipalities').classList.remove('hidden');
 		document.querySelector('#groups').classList.remove('hidden');
 	} else if (screen === 'participants') {
 		document.querySelector('main#selection').classList.remove('hidden');
+		document.querySelector('#about').classList.remove('hidden');
 		document.querySelector('#states').classList.remove('hidden');
 		document.querySelector('#municipalities').classList.remove('hidden');
 		document.querySelector('#groups').classList.remove('hidden');
@@ -72,13 +104,77 @@ function updateScreen(screen) {
 		document.querySelector('main#results').classList.remove('hidden');
 
 
+		// const industriesList = document.createElement('ul');
+		// _data[_state].industries.forEach((industry) => {
+		// 	const industryItem = document.createElement('li');
+		// 	industryItem.textContent = industry;
+		// 	industriesList.appendChild(industryItem);
+		// });
+
+		const blockades = _data[_state].municipalities[_municipality].claim_groups[_group].entries;
+		const blockadesList = document.createElement('ul');
+
+		blockades.forEach((blockade, i) => {
+			const blockadeItem = document.createElement('li');
+			blockadeItem.insertAdjacentHTML( 'beforeend', `
+				<div class="blockade">
+					<div><a class="road" href="${blockade.road}" data-blockadeindex="${i}">${blockade.road}</a></div>
+					<div>${blockade.date}</div>
+					<div>${blockade.num_participants} participants</div>
+				</div>	
+			`);
+			blockadesList.appendChild(blockadeItem);			
+		});
+
 		document.querySelector('#result-state').textContent = _state;
+		// document.querySelector('#result-industries').appendChild(industriesList);
 		document.querySelector('#result-industries').textContent = _data[_state].industries.join(', ');
 		document.querySelector('#result-municipality').textContent = _municipality;
 		document.querySelector('#result-group').textContent = _group;
 		document.querySelector('#result-participants').textContent = _numParticipants;
-		document.querySelector('#result-months	').textContent = _numMonths;
+		document.querySelector('#result-days').textContent = _numDays;
+		document.querySelector('#result-material').textContent = _material;
+		document.querySelector('#result-damage').textContent = _damage;
+		document.querySelector('#result-blockades').appendChild(blockadesList);
+
+		document.querySelectorAll('#result-blockades .blockade a.road').forEach((roadLink) => {
+			roadLink.addEventListener('click', (e) => {
+				e.preventDefault();
+				const idx = e.target.dataset.blockadeindex;
+				showBlockade(idx)				
+			});
+		});
+
+		showBlockade(0);
 	}
+}
+
+function showBlockade(idx) {
+	const blockade = _data[_state].municipalities[_municipality].claim_groups[_group].entries[idx];
+	
+	document.querySelector('#blockade-road').textContent = blockade.road;
+	document.querySelector('#blockade-grievance').textContent = blockade.type_of_claim_en;
+	document.querySelector('#blockade-people').textContent = blockade.num_participants;
+	document.querySelector('#blockade-date').textContent = blockade.date;
+
+	console.log(_images.images);
+	const img = new Image();
+
+
+
+	img.addEventListener("error", () => {
+		document.querySelector('.column.blockade').style.backgroundImage = `url(/images/${_images.images[Math.floor(Math.random() * _images.images.length)]}.png)`;
+	});
+	img.addEventListener("load", () => {
+		document.querySelector('.column.blockade').style.backgroundImage = `url(/images/${blockade.road}.png)`;
+	});
+	img.src = `/images/${blockade.road}.png`;
+
+
+
+	// /images/AM-254.png
+	// document.querySelector('.column.blockade').style.backgroundImage = 'url(/images/AM-254.png)';
+	
 }
 
 function createStates() {
@@ -90,11 +186,14 @@ function createStates() {
 
 	for (let state in _data) {
 		const numBlockades = _data[state].num_entries;
+		let label = (numBlockades > 1) ? 'blockades' : 'blockade';
+
 		const claimGroups = _data[state].claim_groups_list.join(', ');
 		const item = document.createElement('li');
 		item.insertAdjacentHTML( 'beforeend', `
 			<div>
 				<div><a href="${state}" data-state="${state}">${state}</a></div>
+				<div>${numBlockades} ${label}</div>
 			</div>
 		`);
 
@@ -124,11 +223,14 @@ function createMunicipalities() {
 		const municipalityData = _data[_state].municipalities[municipality];
 
 		const numBlockades = municipalityData.num_entries;
+		let label = (numBlockades > 1) ? 'blockades' : 'blockade';
+
 		const claimGroups = municipalityData.claim_groups_list.join(', ');
 		const item = document.createElement('li');
 		item.insertAdjacentHTML( 'beforeend', `
 			<div>
 				<div><a href="${municipality}" data-municipality="${municipality}">${municipality}</a></div>
+				<div>${numBlockades} ${label}</div>
 			</div>
 		`);
 
@@ -160,10 +262,11 @@ function createClaimGroups() {
 		const numBlockades = groupData.num_entries;
 		const claimGroups = groupData.claim_groups_list.join(', ');
 		const item = document.createElement('li');
+		let label = (numBlockades > 1) ? 'blockades' : 'blockade';
 		item.insertAdjacentHTML( 'beforeend', `
 			<div>
 				<div><a href="${group}" data-group="${group}">${group}</a></div>
-				<div>Blockades: ${numBlockades}</div>
+				<div>${numBlockades} ${label}</div>
 			</div>
 		`);
 
@@ -175,11 +278,8 @@ function createClaimGroups() {
 			e.preventDefault();
 			const group = e.target.dataset.group;
 			_group = group;
-			console.log(group);
-
 			updateScreen('participants');
 			// createClaimGroups();
-
 		});
 	});	
 	updateScreen('group');
