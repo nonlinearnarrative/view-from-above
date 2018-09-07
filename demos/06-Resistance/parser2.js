0423a762028d6c31a6307409c33c75fa71290431
Daniel Powers <dancepowers@gmail.com>
2018-09-07 10:01:45 +0200

    blockades data

--- demos/06-Resistance/parser2.js
+++ demos/06-Resistance/parser2.js
@@ -10,6 +10,7 @@ const data = {};
 csvRows.forEach((row, num) => {
 	const state = row[0];
 	const municipality = row[1];
+	const claim_group = row[10];
 
 	if (num > 0) {
 		const entry = {
@@ -28,19 +29,58 @@ csvRows.forEach((row, num) => {
 			type_of_violence_en: row[12],
 		};
 
-		
-
-		if (data[municipality] === undefined) {
-			data[municipality] = {
-				municipality: municipality,
-				actions: [],
+		if (data[state] === undefined) {
+			data[state] = {
+				num_entries: 0,
+				claim_groups_list: [],
+				municipalities: {}
 			}
 		}
 
-		data[municipality].actions.push(entry);
+		if (data[state].municipalities[municipality] === undefined) {
+			data[state].municipalities[municipality] = {
+				num_entries: 0,
+				claim_groups_list: [],
+				claim_groups: {},
+			}
+		}
+
+		if (data[state].municipalities[municipality].claim_groups[claim_group] === undefined) {
+			data[state].municipalities[municipality].claim_groups[claim_group] = {
+				num_entries: 0,
+				claim_groups_list: [],
+				entries: [],
+			};
+		}
+
+		data[state].municipalities[municipality].claim_groups[claim_group].entries.push(entry);
 	}
 
 });
 
+for (let state in data) {
+
+	for (let municipality in data[state].municipalities) {
+		for (let claim_group in data[state].municipalities[municipality].claim_groups) {
+
+			if (data[state].claim_groups_list.indexOf(claim_group) < 0) {
+				data[state].claim_groups_list.push(claim_group);	
+			}
+
+			if (data[state].municipalities[municipality].claim_groups_list.indexOf(claim_group) < 0) {
+				data[state].municipalities[municipality].claim_groups_list.push(claim_group);	
+			}
+			
+			if (data[state].municipalities[municipality].claim_groups[claim_group].claim_groups_list.indexOf(claim_group) < 0) {
+				data[state].municipalities[municipality].claim_groups[claim_group].claim_groups_list.push(claim_group);	
+			}
+
+			data[state].num_entries += data[state].municipalities[municipality].claim_groups[claim_group].entries.length;	
+			data[state].municipalities[municipality].num_entries += data[state].municipalities[municipality].claim_groups[claim_group].entries.length;
+			data[state].municipalities[municipality].claim_groups[claim_group].num_entries += data[state].municipalities[municipality].claim_groups[claim_group].entries.length;
+		}
+	}
+}
+
 const json = JSON.stringify(data);
-fs.writeFileSync('data.json', json, 'utf8');
+fs.writeFileSync('blockades.json', json, 'utf8');
